ca522fd18a72be74858ca53cc1a2d8d56753732c
karolis98 <karolis.980821@gmail.com>
2024-06-26 23:17:02 +0300

    creating HomePage

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      AboutPage
+      <Link to="/">Back</Link>
+    </div>
+  );
 };
 
 export default AboutPage;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./HomePageStyle.css";
+import { Link } from "react-router-dom";
+import Photo from "../../assets/photos/Photo.png";
+import Background from "../../assets/Background.jpg";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      {/* <img className="Photo" src={Photo} alt="No Photo" /> */}
+      <img className="Photo-bg" src={Background} alt="No Photo" />
+      <img className="Photo" src={Photo} alt="No Photo" />
+      <Link to="/about">about</Link>;
+    </div>
+  );
 };
 
 export default HomePage;
